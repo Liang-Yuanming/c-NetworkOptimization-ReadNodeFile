676ec948fac8c910b772ad0866bd7647b33f9f39
mike-mac <w87754@gmail.com>
2016-03-19 23:49:54 +0800

    Initial Commit

---
+++ network_hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  network_hw1
+//
+//  Created by mike on 2016/3/19.
+//  Copyright © 2016年 Naction Cheng Kung University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
